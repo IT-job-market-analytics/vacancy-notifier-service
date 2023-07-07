aeef19442b95967bcb726fade5fc18b7ec1be026
Dmitriy Sapronov <dimasapronov31@gmail.com>
2023-07-07 20:03:57 +0000

    feat: created Spring Boot 3 & Java 17 project with AMQP and web modules

---
+++ src/main/java/com/example/vacancynotifierservice/VacancyNotifierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacancynotifierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyNotifierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyNotifierServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vacancynotifierservice/VacancyNotifierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacancynotifierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyNotifierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
