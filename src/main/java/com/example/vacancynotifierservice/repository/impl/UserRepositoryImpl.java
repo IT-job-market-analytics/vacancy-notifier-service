976d8430f69c2bcd528dce33b87ea73a857e2d78
Dmitriy Sapronov <dimasapronov31@gmail.com>
2023-07-31 09:53:54 +0000

    feat: implemented message sending to all subscribed users

---
+++ src/main/java/com/example/vacancynotifierservice/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.example.vacancynotifierservice.repository.impl;
+
+import com.example.vacancynotifierservice.dto.telegram.UserDto;
+import com.example.vacancynotifierservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@RequiredArgsConstructor
+@Repository
+@Slf4j
+public class UserRepositoryImpl implements UserRepository {
+    private final RestTemplate restTemplate;
+    private final String URI = "http://localhost:8080/users/byQuery/";
+    @Override
+    public List<UserDto> findUsersByQuery(String query) {
+        String fullURI = URI.concat(query);
+
+        UserDto[] userArray = restTemplate.getForObject(fullURI,UserDto[].class);
+        return Arrays.stream(userArray).toList();
+
+
+    }
+}
--- src/main/java/com/example/vacancynotifierservice/service/ConsumerService.java
+++ src/main/java/com/example/vacancynotifierservice/service/ConsumerService.java
@@ -1,18 +1,49 @@
 package com.example.vacancynotifierservice.service;
 
 import com.example.vacancynotifierservice.dto.hh.Vacancy;
+import com.example.vacancynotifierservice.dto.telegram.TelegramNotificationTaskDTO;
+import com.example.vacancynotifierservice.dto.telegram.UserDto;
+import com.example.vacancynotifierservice.repository.UserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ConsumerService {
     private ObjectMapper objectMapper = new ObjectMapper();
+    private final UserRepository userRepository;
+    private final ProducerService producerService;
     @RabbitListener(queues = "${rabbitmq.consumer.new-vacancies-queue}")
     public void consumeVacancy(Vacancy vacancy) throws JsonProcessingException {
+        List<UserDto> users = userRepository.findUsersByQuery(vacancy.getQuery());
+        sendMessagesToTelegram(users, getMessage(vacancy));
         log.info("Message consumed: " + objectMapper.writeValueAsString(vacancy));
+    }
+
+    private void sendMessagesToTelegram(List<UserDto> users, String message){
+        for (UserDto user:
+             users) {
+            try {
+                sendMessage(user.getTelegramChatId(), message);
+            }catch (NullPointerException ex){
+                log.error(ex.getMessage());
+            }
+
+        }
+    }
+    private void sendMessage(long chatId, String message){
+        TelegramNotificationTaskDTO telegramNotificationTaskDTO = new TelegramNotificationTaskDTO(chatId, message);
+        producerService.sendNotification(telegramNotificationTaskDTO);
+        log.info("Notification task sent");
+    }
+    private String getMessage(Vacancy vacancy){
+        return "test";
     }
 }
