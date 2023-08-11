52cfdc23a5bd0139fbf799f9084268f52d1fdcee
Sergey Zhukov <zhukovsd@gmail.com>
2023-08-11 20:11:16 +0400

    fix: rest-api host moved to config instead of being hardcoded as localhost

--- src/main/java/com/example/vacancynotifierservice/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/example/vacancynotifierservice/repository/impl/UserRepositoryImpl.java
@@ -2,26 +2,31 @@ package com.example.vacancynotifierservice.repository.impl;
 
 import com.example.vacancynotifierservice.dto.telegram.UserDto;
 import com.example.vacancynotifierservice.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-@RequiredArgsConstructor
+
 @Repository
 @Slf4j
 public class UserRepositoryImpl implements UserRepository {
     private final RestTemplate restTemplate;
-    private final String URI = "http://localhost:8080/users/byQuery/";
+    private final String restApiHost;
+
+    public UserRepositoryImpl(RestTemplate restTemplate, @Value("${rest-api.host}") String restApiHost) {
+        this.restTemplate = restTemplate;
+        this.restApiHost = restApiHost;
+    }
+
+    private final String endpoint = "/users/byQuery/";
     @Override
     public List<UserDto> findUsersByQuery(String query) {
-        String fullURI = URI.concat(query);
+        String url = restApiHost.concat(endpoint).concat(query);
 
-        UserDto[] userArray = restTemplate.getForObject(fullURI,UserDto[].class);
+        UserDto[] userArray = restTemplate.getForObject(url, UserDto[].class);
         return Arrays.stream(userArray).toList();
     }
 }
